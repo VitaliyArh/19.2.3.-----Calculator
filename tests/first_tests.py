588e39d5705112ec5e4cb51d51874ee50ab4f070
VitaliyArh <vita173@yandex.ru>
2022-11-14 23:29:02 +0200

    Update first_tests.py

--- tests/first_tests.py
+++ tests/first_tests.py
@@ -5,17 +5,17 @@ class TestCalc:
     def setup(self):
         self.calc = Calculator
 
-    def test_multiply_calculate_correctly(self):
-        assert self.calc.multiply(self, 2, 2)
+    def test_multiply_calculate_correctly(self):       # метод умножения
+        assert self.calc.multiply(self, 2, 2) == 4
 
-    def test_division_calculate_correctly(self):
-        assert  self.calc.division(self, 6, 2)
+    def test_division_calculate_correctly(self):       # метод деления
+        assert  self.calc.division(self, 6, 2) == 3
 
-    def test_subtraction_calculate_correctly(self):
-        assert  self.calc.subtraction(self, 5, 2)
+    def test_subtraction_calculate_correctly(self):    # метод вычитания
+        assert  self.calc.subtraction(self, 5, 2) == 3
 
-    def test_adding_calculate_correctly(self):
-        assert  self.calc.adding(self, 4, 2)
+    def test_adding_calculate_correctly(self):         # метод сложения
+        assert  self.calc.adding(self, 4, 2) == 6
 
-    def test_adding_failed(self):
+    def test_adding_failed(self):                      # метод сложения - негативный
         assert self.calc.adding(self, 2, 2) == 5
